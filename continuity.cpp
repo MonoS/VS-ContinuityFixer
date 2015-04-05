c73fda0e952f5f7a85da15d4af3e35911c5706df
MonoS94 <MonoS94@gmail.com>
2015-04-06 01:09:22 +0200

    Finally added support for processing all the planes in a single call.
    left, top, right, bottom and radius now are a vector of at most 3 arguments [why you should have more than 3 planes?!?!], one for each plane.

--- continuity.cpp
+++ continuity.cpp
@@ -16,12 +16,12 @@ typedef struct
     VSNodeRef *node;
     const VSVideoInfo *vi;
 
-    int left;
-    int right;
-    int top;
-    int bottom;
+    int left[3];
+    int right[3];
+    int top[3];
+    int bottom[3];
 
-    int radius;
+    int radius[3];
 } ContinuityData;
 
 void least_squares(int n, least_squares_data *d, double *a, double *b)
@@ -121,52 +121,57 @@ static const VSFrameRef  *VS_CC continuityGetFrame(int n, int activationReason, 
     else if (activationReason == arAllFramesReady)
     {
         const VSFrameRef *src = vsapi->getFrameFilter(n, d->node, frameCtx);
-
-        int height = vsapi->getFrameHeight(src, 0);
-        int width = vsapi->getFrameWidth(src, 0);
-        int stride = vsapi->getStride(src, 0);
-		int bytesPerSample = vsapi->getFrameFormat(src)->bytesPerSample;
-
-        least_squares_data *buf = (least_squares_data*) malloc(required_buffer(width > height ? width : height));
-		
 		VSFrameRef *dst = vsapi->copyFrame(src, core);
 		
-        uint8_t *dest = vsapi->getWritePtr(dst, 0);
+		int h = vsapi->getFrameHeight(src, 0);
+		int w = vsapi->getFrameWidth(src, 0);
 		
+		least_squares_data *buf = (least_squares_data*) malloc(required_buffer(w > h ? w : h));
+		
+		int bytesPerSample = vsapi->getFrameFormat(src)->bytesPerSample;
 		process_function proc;
 		if (bytesPerSample == 1)
 			proc = process_edge<uint8_t>;
 		else
 			proc = process_edge<uint16_t>;
+		
+		for(int j = 0; j < d->vi->format->numPlanes; ++j)
+		{
+			int height = vsapi->getFrameHeight(src, j);
+			int width = vsapi->getFrameWidth(src, j);
+			int stride = vsapi->getStride(src, j);
+			
+			uint8_t *dest = vsapi->getWritePtr(dst, j);
 
-        int i;
-        // top
-        for (i = 0; i < d->top; ++i)
-        {
-            int ref_row = d->top - i;
-            proc(dest + stride * (ref_row - 1), dest + stride * ref_row, bytesPerSample, bytesPerSample, width, d->radius, buf);
-        }
+			int i;
+			// top
+			for (i = 0; i < d->top[j]; ++i)
+			{
+				int ref_row = d->top[j] - i;
+				proc(dest + stride * (ref_row - 1), dest + stride * ref_row, bytesPerSample, bytesPerSample, width, d->radius[j], buf);
+			}
 
-        // bottom
-        for (i = 0; i < d->bottom; ++i)
-        {
-            int ref_row = height - d->bottom - 1 + i;
-            proc(dest + stride * (ref_row + 1), dest + stride * ref_row, bytesPerSample, bytesPerSample, width, d->radius, buf);
-        }
+			// bottom
+			for (i = 0; i < d->bottom[j]; ++i)
+			{
+				int ref_row = height - d->bottom[j] - 1 + i;
+				proc(dest + stride * (ref_row + 1), dest + stride * ref_row, bytesPerSample, bytesPerSample, width, d->radius[j], buf);
+			}
 
-        // left
-        for (i = 0; i < d->left; ++i)
-        {
-            int ref_col = d->left - i;
-            proc(dest + (ref_col - 1) * bytesPerSample, dest + ref_col * bytesPerSample, stride, stride, height, d->radius, buf);
-        }
+			// left
+			for (i = 0; i < d->left[j]; ++i)
+			{
+				int ref_col = d->left[j] - i;
+				proc(dest + (ref_col - 1) * bytesPerSample, dest + ref_col * bytesPerSample, stride, stride, height, d->radius[j], buf);
+			}
 
-        // right
-        for (i = 0; i < d->right; ++i)
-        {
-            int ref_col = width - d->right - 1 + i;
-            proc(dest + (ref_col + 1) * bytesPerSample, dest + ref_col * bytesPerSample, stride, stride, height, d->radius, buf);
-        }
+			// right
+			for (i = 0; i < d->right[j]; ++i)
+			{
+				int ref_col = width - d->right[j] - 1 + i;
+				proc(dest + (ref_col + 1) * bytesPerSample, dest + ref_col * bytesPerSample, stride, stride, height, d->radius[j], buf);
+			}
+		}
         
         free(buf);
         vsapi->freeFrame(src);
@@ -199,17 +204,76 @@ static void VS_CC  continuityCreate(const VSMap *in, VSMap *out, void *userData,
         vsapi->freeNode(d.node);
         return;
     }
-
-    d.bottom = vsapi->propGetInt(in, "bottom", 0, &err);
-    d.top =    vsapi->propGetInt(in, "top", 0, &err);
-    d.right =  vsapi->propGetInt(in, "right", 0, &err);
-    d.left =   vsapi->propGetInt(in, "left", 0, &err);
-
-    d.radius = vsapi->propGetInt(in, "radius", 0, &err);
-    if (err)
+	
+	int nPlanes = d.vi->format->numPlanes;
+	int nLeft = vsapi->propNumElements(in, "left");
+	int nTop = vsapi->propNumElements(in, "top");
+	int nRight = vsapi->propNumElements(in, "right");
+	int nBottom = vsapi->propNumElements(in, "bottom");
+	int nRadius = vsapi->propNumElements(in, "radius");
+	
+	if(nPlanes > 3)
+	{
+		vsapi->setError(out, "ContinuityFixer: Too many planes in the source clip");
+        vsapi->freeNode(d.node);
+        return;
+	}	
+	
+	if(nLeft > nPlanes)
+	{
+		vsapi->setError(out, "ContinuityFixer: Too many left parameter");
+        vsapi->freeNode(d.node);
+        return;
+	}
+	if(nTop > nPlanes)
+	{
+		vsapi->setError(out, "ContinuityFixer: Too many top parameter");
+        vsapi->freeNode(d.node);
+        return;
+	}
+	if(nRight > nPlanes)
+	{
+		vsapi->setError(out, "ContinuityFixer: Too many right parameter");
+        vsapi->freeNode(d.node);
+        return;
+	}
+	if(nBottom > nPlanes)
+	{
+		vsapi->setError(out, "ContinuityFixer: Too many bottom parameter");
+        vsapi->freeNode(d.node);
+        return;
+	}
+	if(nRadius > nPlanes)
+	{
+		vsapi->setError(out, "ContinuityFixer: Too many radius parameter");
+        vsapi->freeNode(d.node);
+        return;
+	}
+	
+	for (int i = 0; i < nPlanes; ++i)
     {
-        d.radius = d.vi->height < d.vi->width ? d.vi->height : d.vi->width;
-    }
+		d.left[i] = vsapi->propGetInt(in, "left", i, &err);
+		d.top[i] = vsapi->propGetInt(in, "top", i, &err);
+		d.right[i] = vsapi->propGetInt(in, "right", i, &err);
+		d.bottom[i] = vsapi->propGetInt(in, "bottom", i, &err);
+	}
+	
+	for (int i = 0; i < nPlanes; ++i)
+    {
+		d.radius[i] = vsapi->propGetInt(in, "radius", i, &err);
+    
+		if (err)
+		{
+			if(i == 0)
+			{
+				d.radius[i] = d.vi->height < d.vi->width ? d.vi->height : d.vi->width;
+			}
+			else
+			{
+				d.radius[i] = (d.vi->height >> d.vi->format->subSamplingH) < (d.vi->width >> d.vi->format->subSamplingW) ? (d.vi->height >> d.vi->format->subSamplingH) : (d.vi->width >> d.vi->format->subSamplingW);
+			}
+		}
+	}
 	
     data = (ContinuityData*) malloc(sizeof(d));
     *data = d;
@@ -221,10 +285,10 @@ VS_EXTERNAL_API(void) VapourSynthPluginInit(VSConfigPlugin configFunc, VSRegiste
 {
     configFunc("com.monos.edgefixer", "edgefixer", "VapourSynth edgefixer port", VAPOURSYNTH_API_VERSION, 1, plugin);
     registerFunc("ContinuityFixer", "clip:clip;"
-	                                "left:int;"
-									"top:int;"
-									"right:int;"
-									"bottom:int;"
-									"radius:int:opt;",
+	                                "left:int[];"
+									"top:int[];"
+									"right:int[];"
+									"bottom:int[];"
+									"radius:int[]:opt;",
 									continuityCreate, 0, plugin);
 }
